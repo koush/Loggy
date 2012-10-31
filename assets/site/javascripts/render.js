95ec6d13ab51bf1de9179f634cdef94222b7d2c8
Koushik Dutta <koushd@gmail.com>
2012-10-31 14:47:36 -0700

    pull header into separate jade

--- assets/site/javascripts/render.js
+++ assets/site/javascripts/render.js
@@ -4,8 +4,29 @@
       jade.render(data, args, fn);
     })
   }
+  $(document).ready(function() {
+	  var templates = $('.template');
+	  var pending = templates.length;
+	  var views = {};
+	  
+	  function render() {
+		  if (pending != 0)
+			  return;
+		  for (var view in views) {
+			  var rendered = views[view];
+			  $('#' + view).html(rendered);
+		  }
+	  }
+	  
+	  $.each(templates, function(index, element) {
+		  var view = 'views/' + element.id + '.jade';
+		  
+		  jade.renderFile(view, {}, function(err, rendered) {
+			  views[element.id] = rendered;
+			  pending--;
+			  render();
+		  });
+	  });
+  });
 })();
 
-jade.renderFile(view, {}, function(err, index) {
-  $('body').html(index);
-})
--- src/com/koushikdutta/loggy/LoggyService.java
+++ src/com/koushikdutta/loggy/LoggyService.java
@@ -75,9 +75,15 @@ public class LoggyService extends Service {
                 }
 
                 if (file.isDirectory()) {
-                    StringBuilder b = new StringBuilder();
-                    b.append(String.format("<html><title>Loggy</title><head><script>WEB_SOCKET_SWF_LOCATION='/web-socket-js/WebSocketMain.swf';</script><link rel='stylesheet' href='/stylesheets/style.css'></link><link rel='stylesheet' href='/bootstrap/css/bootstrap.min.css'></link><link rel='stylesheet' href='/bootstrap/css/bootstrap-responsive.min.css'></link><script>view = '/views/%s.jade';</script><script src='/javascripts/jquery-1.8.1.min.js'></script><script src='/bootstrap/js/bootstrap.js'></script><script src='/javascripts/jade.min.js'></script><script src='/javascripts/render.js'></script></head><body></body></html>", view));
-                    response.send(b.toString());
+                    try {
+                        if (mLayout == null)
+                            mLayout = readFully(AsyncHttpServer.getAssetStream(LoggyService.this, "layout.html"));
+                    }
+                    catch (IOException e) {
+                        response.send("internal error");
+                        return;
+                    }
+                    response.send(String.format(mLayout, view));
                     return;
                 }
                 
